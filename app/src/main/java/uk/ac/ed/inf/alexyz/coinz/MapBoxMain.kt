01fd6172be33acc9b2bc851918f224722d5bdd8a
alexb <s1536094@ed.ac.uk>
2018-10-25 15:47:46 +0100

    Mapbox is now set up with accurate user location and permissions implemented. Plan to continue with geoJSON pins being dropped on map by tomorrow evening.

--- app/src/main/java/uk/ac/ed/inf/alexyz/coinz/MapBoxMain.kt
+++ app/src/main/java/uk/ac/ed/inf/alexyz/coinz/MapBoxMain.kt
@@ -1,16 +1,39 @@
 package uk.ac.ed.inf.alexyz.coinz
 
+import android.annotation.SuppressLint
+import android.location.Location
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import com.mapbox.android.core.location.LocationEngine
+import com.mapbox.android.core.location.LocationEngineListener
+import com.mapbox.android.core.location.LocationEnginePriority
+import com.mapbox.android.core.location.LocationEngineProvider
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.RenderMode
 
 import kotlinx.android.synthetic.main.activity_map_box_main.*
+import java.security.Permission
+import org.jetbrains.anko.toast
 
-class MapBoxMain : AppCompatActivity() {
+class MapBoxMain : AppCompatActivity(), PermissionsListener, LocationEngineListener {
+
 
     private lateinit var mapView: MapView
+    private lateinit var map: MapboxMap
+    private lateinit var permissionManager: PermissionsManager
+    private lateinit var originLocation: Location
+
+    private var locationEngine: LocationEngine? = null
+    private var locationLayerPlugin: LocationLayerPlugin? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +42,106 @@ class MapBoxMain : AppCompatActivity() {
         Mapbox.getInstance(applicationContext, getString(R.string.access_token))
         mapView = findViewById(R.id.mapView)
         mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync{ mapboxMap ->
+            map = mapboxMap
+            enableLocation()
+        }
+    }
 
+    fun enableLocation(){
+        if (PermissionsManager.areLocationPermissionsGranted(this)){
+            initialiseLocationEngine()
+            initialiseLocationLayer()
+        } else {
+            permissionManager = PermissionsManager(this)
+            permissionManager.requestLocationPermissions(this)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun initialiseLocationEngine(){
+        locationEngine = LocationEngineProvider(this).obtainBestLocationEngineAvailable()
+        locationEngine?.priority = LocationEnginePriority.HIGH_ACCURACY
+        locationEngine?.activate()
+
+        val lastLocation = locationEngine?.lastLocation
+        if (lastLocation != null){
+            originLocation = lastLocation
+            setCameraPosition(lastLocation)
+        } else {
+            locationEngine?.addLocationEngineListener(this)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun initialiseLocationLayer(){
+        locationLayerPlugin = LocationLayerPlugin(mapView,map,locationEngine)
+        locationLayerPlugin?.setLocationLayerEnabled(true)
+        locationLayerPlugin?.cameraMode = CameraMode.TRACKING
+        locationLayerPlugin?.renderMode = RenderMode.NORMAL
 
     }
 
+    private fun setCameraPosition(location: Location){
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(location.latitude, location.longitude), 14.0))
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onConnected() {
+        locationEngine?.requestLocationUpdates()
+    }
+
+    override fun onLocationChanged(location: Location?) {
+        location?.let {
+            originLocation = location
+            setCameraPosition(location)
+        }
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        if (granted) {
+            enableLocation()
+        }
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+        toast("We require this permission if you wish to collect any coinz")
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        permissionManager.onRequestPermissionsResult(requestCode,permissions,grantResults)
+    }
+
+    @SuppressWarnings("MissingPermission")
+    override fun onStart() {
+        super.onStart()
+        if (PermissionsManager.areLocationPermissionsGranted(this)){
+            locationEngine?.requestLocationUpdates()
+            locationLayerPlugin?.onStart()
+        }
+        mapView.onStart()
+    }
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+    override fun onStop() {
+        super.onStop()
+        locationEngine?.removeLocationUpdates()
+        mapView.onStop()
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        locationEngine?.deactivate()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
 }
